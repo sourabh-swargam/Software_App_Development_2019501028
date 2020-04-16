2719361e32db76419902c115196098fec10b0e79
sourabh-swargam <swargam.sourabh@gmail.com>
2020-04-16 20:00:08 +0530

    created users are stored in database

--- Project - 1/application.py
+++ Project - 1/application.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+import datetime
 
 app = Flask(__name__)
-name_list = []
-# app.static_folder = 'static'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# db.init_app(app)
+# db = SQLAlchemy()
+
+class User(db.Model):
+	__tablename__ = "users"
+	# id = db.Column(db.Serial, primary_key=True)
+	name = db.Column(db.String, primary_key=True, nullable=False)
+	pw = db.Column(db.String, nullable=False)
+	email = db.Column(db.String, nullable=False)
+	timestamp = db.Column(db.Date, nullable=False)
+
+if User.query.all() == []:
+	db.drop_all()
+	db.create_all()
+	db.session.commit()
+
 @app.route("/")
 def index():
 	return render_template("index.html")
@@ -13,6 +35,34 @@ def register():
 	if request.method == "GET":
 		return render_template("register.html")
 	elif request.method == "POST":
-		name_list.append(request.form.get("name"))
-		name_list.sort()
-		return render_template("user_list.html", names=name_list)
+		n = request.form.get("name")
+		pw = request.form.get("pw")
+		timestamp = datetime.datetime.now()
+		mail = request.form.get("email")
+		try:
+			user = User(name=n, timestamp=timestamp, email=mail, pw=pw)
+			db.session.add(user)
+		except:
+			return render_template("error.html")
+		db.session.commit()
+		return render_template("success.html", name=user)
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+	print('here')
+	if request.method == "GET":
+		return render_template("login.html")
+	elif request.method == "POST":
+		n = request.form.get("name")
+		pw = request.form.get("pw")
+		user = User.query.filter_by(name=n).all()
+		if len(user) == 0:
+			return render_template("error.html")
+		print("THIS IS IT",user[0].name)
+		return render_template("success.html", name=user[0])
+
+@app.route("/admin")
+def admin():
+	user_list = User.query.all()
+	user_list.sort(key = lambda x: x.timestamp)
+	return render_template("admin.html", name=user_list)
