57c75fb523d334b60011c3641ccbc60e5563d5c6
sourabh-swargam <swargam.sourabh@gmail.com>
2020-04-29 01:10:50 +0530

    completed day 13 assignment

---
+++ Day 13/init.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=application.py
+set FLASK_DEBUG=1
+set DATABASE_URL=postgres://ynncbhqioxzhou:e3cf6b7b921319efa91644fcebf63e8e3a70bd43e89cc4d8b1d3e5c4661371d6@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dffk2gae9jme7n
+flask run
